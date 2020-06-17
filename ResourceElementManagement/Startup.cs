0c3fb55e5cd4482502ef896367e33c0b5f9c2153
wanghuachuan <497697273@qq.com>
2020-06-17 09:49:47 +0800

    备份

--- ResourceElementManagement/Startup.cs
+++ ResourceElementManagement/Startup.cs
@@ -41,7 +41,7 @@ namespace ResourceElementManagement
                 options.AddPolicy("CustomCorsPolicy", policy =>
                 {
                     // 设定允许跨域的来源，有多个可以用','隔开
-                    policy.WithOrigins("http://localhost:8081","http://localhost:8080", "http://192.168.31.119:8080")
+                    policy.WithOrigins("http://127.0.0.1:8848", "http://localhost:8080", "http://192.168.31.119:8080","http://192.168.137.1:8848")
                     .AllowAnyHeader()
                     .AllowAnyMethod()
                     .AllowCredentials();
